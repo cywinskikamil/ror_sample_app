c0931daf81d3fd5e7db2b9a20d6e3218d6ad1d3f
cywinskikamil <cywinski.kamil@yahoo.com>
2016-06-30 15:14:54 +0200

    finished layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
   test "layout links" do  
-    get help_path
-    assert_template 'static_pages/help'
+    get root_path
+    assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path 
     assert_select "a[href=?]", contact_path  
-    assert_select "title", full_title("Help")
+    assert_select "a[href=?]", signup_path
+    assert_select "title", full_title("")
   end
 end
